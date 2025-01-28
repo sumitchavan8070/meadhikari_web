04b0b75cdfa80a7dadd2a209d578c31c306f78af
Sumit Chavan <sdchavan8070@gmail.com>
2025-01-28 14:27:37 +0530

    url updated

--- utils/globalStrings.js
+++ utils/globalStrings.js
@@ -1,4 +1,4 @@
-// export const BASE_URL = "https://meadhikari-sever.vercel.app/api/v1/auth"; // Replace with your actual API base URL
+export const BASE_URL = "https://meadhikari-sever.vercel.app/api/v1/auth"; // Replace with your actual API base URL
 // export const BASE_URL = "http://localhost:8080/api/v1/auth"; // Replace with your actual API base URL
 
-export const BASE_URL = "http://192.168.43.78:8080/api/v1/auth";
+// export const BASE_URL = "http://192.168.43.78:8080/api/v1/auth";
