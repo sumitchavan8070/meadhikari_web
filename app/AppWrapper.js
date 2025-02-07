2e83cd97f1781f366e5f811756e2de7846a9940c
Sumit Chavan <sdchavan8070@gmail.com>
2025-02-07 21:22:57 +0530

    Test is working or not

--- app/AppWrapper.js
+++ app/AppWrapper.js
@@ -80,7 +80,7 @@ const AppWrapper = ({ children }) => {
     // Set up the interval to show the popup every 15 seconds
     intervalRef.current = setInterval(() => {
       setShowLoginPopup(true); // Open the popup
-    }, 60000); // 30 seconds
+    }, 60000); // 60 seconds
 
     // Cleanup the interval when the component unmounts or the user logs in
     return () => {
